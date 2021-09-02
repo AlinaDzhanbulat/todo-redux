35123b98e6cfaa84e018ff342224272e35ad45ef
AlinaDzhanbulat <a.janbolotovna@gmail.com>
2021-09-02 12:36:37 +0300

    todo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import { storeTesting } from './store/storeTesting/storeTesting'
+
+
+import Footer from './components/Footer/footer'
+import AddTodo from './containers/AddTodo/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList/VisibleTodoList'
+
+export default function App() {
+    //uncomment for testing
+    // storeTesting();
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+        </div>
+    )
+
+}
---
+++ src/store/actions/actions.js
@@ -0,0 +1,40 @@
+/*
+ * типы экшенов
+ */
+
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * другие константы
+ */
+
+// const NOMINAL = ["USD", "SOM", "SYM"];
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * генераторы экшенов
+ */
+
+let nextTodoId = 0
+export const addTodo = text => ({
+    type: ADD_TODO,
+    id: nextTodoId++,
+    text: text
+})
+
+export const setVisibilityFilter = filter => ({
+    type: SET_VISIBILITY_FILTER,
+    filter
+})
+
+export const toggleTodo = id => ({
+    type: TOGGLE_TODO,
+    id: id
+})
